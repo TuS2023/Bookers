7136a4f3b38e3f57deb2d563509b6a09e1044567
TuS2023 <138543864+TuS2023@users.noreply.github.com>
2023-08-08 10:14:52 +0000

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,10 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    
     book.save
-    
     redirect_to book_path(book.id)
   end 
   
-
   def show
     @book = Book.find(params[:id])
   end
@@ -24,6 +21,17 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/'
+  end
   
   private
   
